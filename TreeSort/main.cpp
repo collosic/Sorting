3dd0f4e4d69dafd054735d0e24b2294184a57642
collosic <christian.collosi@gmail.com>
2014-09-28 17:27:54 -0700

    Fixed tabs in vim

--- TreeSort/main.cpp
+++ TreeSort/main.cpp
@@ -1,14 +1,17 @@
 // Example main.cpp on how to use treesort.h
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 #include "treesort.h"
+
 using namespace std;
 
 int main() {
-	int C[] = {5, 8, 14, 2, 6, 11, 6, 9};
-	int arrSize = sizeof(C) / sizeof(*C);
+	srand(time(NULL));
+	int size = 20;
 	BinarySearchTree treeSort;
-	for (int i = 0; i < arrSize; i++) {
-		treeSort.insertEntry(C[i]);
+	for (int i = 0; i < size; i++) {
+		treeSort.insertEntry(rand() % 101);
 	}
 	treeSort.displayTree(true);
 	return 0;
